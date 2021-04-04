8113bd0a8cdeb88a1ffb87cc26679a149574e888
fwipsity <fwipsity@gmail.com>
2021-04-04 20:51:59 +0300

    finished config loading

--- main.ts
+++ main.ts
@@ -1,7 +1,26 @@
 const tmi = require("tmi.js");
 
 class Bot{
-    constructor(){
-        
+    private client: any;
+    constructor(config: botConfig){
+        this.client = new tmi.Client({
+            options: {
+                debug: config.debug || false,
+                messagesLogLevel: config.messagesLogLevel || "info" 
+            },
+            connection: {
+                reconnect: config.reconnect || true,
+                secure: config.secure || true
+            },
+            identity: {
+                username: config.username,
+                password: config.password
+            },
+            channels: config.channels
+        });
+        if(config.color){
+            this.client.color(config.color);
+        }
+        this.client.connect();
     }
 }
